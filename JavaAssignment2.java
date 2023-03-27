6bb82a1981dec9c0184cf249bca0e43e1dd57946
Gitdurgabhavni <durgabhavani33312@gmail.com>
2023-03-27 19:33:36 +0530

    first commit

---
+++ JavaAssignment2.java
@@ -0,0 +1,35 @@
+1.What is statically typed and dynamically typed programming language?
+Ans.In statically type language we have to specify the type of the data the compile time itself. Where as in the dynamacilly type language type of the 
+data can be checked at the runtime. 
+
+2.What is variable in java?
+Ans. Variable in java is used to store the data or the information.
+
+3.How to assign a value to the variable?
+Ans. data type variable_name= value;
+
+4.What are Primitive data types in java?
+Ans.Primitive data type include byte,short,int,long,float,double,boolean,char.
+
+5.What are identifiers in java?
+Ans.Identifier is a name give to the package,class,interface,method or variable.
+
+6.List the operators in java?
+Ans.Operators in Java can be classified into 6 types:
+Arithmetic Operators.
+Assignment Operators.
+Relational Operators.
+Logical Operators.
+Unary Operators.
+Bitwise Operators.
+
+7.Explain about the increment and decrement operator and give an example.
+Ans. the increment operator ++ increases the value of a variable by 1.
+Ex:int a=5;
+    a++;
+    o/p=6
+
+ Similarly the decrement operator -- decreases the value of a variable by 1. 
+  Ex:a=5;
+     a--;
+      o/p=5
